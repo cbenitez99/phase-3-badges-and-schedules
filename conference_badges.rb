fe90f1c8d8c3412315895139a7e648f938b5b40a
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-09-20 21:37:35 -0500

    commit tree 5007a39346a15d419f86e5b022ab9267cd46e64b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+    array_of_names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |value, index|
+        "Hello, #{value}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each {|speaker| puts speaker}
+    assign_rooms(speakers).each {|speaker| puts speaker}
+end
